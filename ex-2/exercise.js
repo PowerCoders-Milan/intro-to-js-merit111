e32555754c5ff76aaa3a0b3046aeaf9dfcd01b78
Your Name Here <ebrimaj604@gmail.com>
2021-02-25 14:44:59 +0100

    ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,6 @@
 // Example
 window.alert("Hello World!");
+var birthYear = 1992;
+var futureYear  = 2021;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1));
