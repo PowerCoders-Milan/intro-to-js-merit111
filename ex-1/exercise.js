0d84e492daf941b3358a96dff9d4658adfa2efcf
Your Name Here <ebrimaj604@gmail.com>
2021-02-25 12:13:07 +0100

    ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,9 @@
 // Example
 window.alert("Hello World!");
+// The Fortune Teller
+var children = 3;
+var partner =" Jasmine";
+var place =" Munich";
+var job = " Manager";
+
+console.log( "You will be a"+ job + " in " +place+ " and married to" +partner+ " with " +children+ " kids")
