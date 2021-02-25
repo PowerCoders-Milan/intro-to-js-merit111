0003b63207f6dd25a1587c359ff54fee2b827d1e
Your Name Here <ebrimaj604@gmail.com>
2021-02-25 17:26:57 +0100

    ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,17 @@
 // Example
-window.alert("Hello World!");
+window.alert("Hello World")
+var insert = prompt('Please enter your Phrases')
+alert(insert  + ' Have ' +insert.length+ 'number of characters');
+
+
+alert('Can i have two different phrase')
+var insertPhrases1 = prompt('Please enter your phrases')
+
+var insertPhrases2 = prompt('please enter your second Phrases')
+
+if (insertPhrases1.length > insertPhrases2.length){
+    alert(insertPhrases1 + 'Phrases is the longest with' + insertPhrases1.length + 'number of characters')
+} 
+ else if (insertPhrases2.length > insertPhrases1.length) {
+    alert(insertPhrases2 + 'Phrases is the longest with' + insertPhrases2.length + 'number of characters')
+}
