82cac990b949d75b563a6d8604053fd06762ccb1
Your Name Here <ebrimaj604@gmail.com>
2021-02-25 16:02:45 +0100

    ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,10 @@
 // Example
-window.alert("Hello World!");
+var number1 = 15;
+var number2 = 15;
+if (number1 > number2) {
+     console.log('The greater number of '+number1+' and '+number2+' is '+number1);
+}  else if (number2 > number1) {
+    console.log('The greater number of '+number1+' and '+number2+' is '+number2);
+} else {
+  console.log ("The numbers are the same.")
+}
